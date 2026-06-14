2f52f4c7ce50c5210cca3e46e6eb00685537db06
Brangetop <brangetop360@gmail.com>
2026-06-15 00:36:42 +0300

    Added Task structure

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import "time"
+
+type Task struct {
+	Name        string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
 func main() {
 
 }
